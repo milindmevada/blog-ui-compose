034621b29021f552ac795e0deea7c0194ec79077
Milind Mevada <milindmevada@gmail.com>
2021-05-13 13:44:36 +0530

    Scaffold Compose Application.

---
+++ app/src/main/java/demo/milind/blogapplication/MainActivity.kt
@@ -0,0 +1,38 @@
+package demo.milind.blogapplication
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import demo.milind.blogapplication.ui.theme.BlogApplicationTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            BlogApplicationTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    BlogApplicationTheme {
+        Greeting("Android")
+    }
+}
